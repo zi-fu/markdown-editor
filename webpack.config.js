e73fa97bc0c1f41c741661d4a32926be39938c20
Masaki Gibo <defi13y@gmail.com>
2022-03-06 15:34:26 +0900

    test

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.tsx',
+  module:{
+    rules:[
+      {
+        test: /\.tsx$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions:['.js', '.ts', '.tsx'],
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+};
+
+
